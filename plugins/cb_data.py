c89e5e585e61be7c1b6e9aa4242c2625c62ba83c
WDS Projects <113333418+WdsProjects@users.noreply.github.com>
2022-10-23 14:38:41 -0300

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -130,7 +130,8 @@ async def vid(bot,update):
      new_name = update.message.text
      used_ = find_one(update.from_user.id)
      used = used_["used_limit"]
-     date = used_["date"]
+     date = used_["date"]	
+     name = new_name.split(":-")
      new_filename = name[1]
      file_path = f"downloads/{new_filename}"
      message = update.message.reply_to_message
